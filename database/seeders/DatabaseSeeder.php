e20ff9b0e5f229cd6b8188177db158f430ffbfee
artparesse <b.k.domarecki@gmail.com>
2026-03-11 21:32:11 +0100

    chore: add database seeder with test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,25 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Book;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'password',
         ]);
+
+        $authors = Author::factory()->count(15)->create();
+
+        Book::factory()->count(100)->create()->each(function (Book $book) use ($authors): void {
+            $book->authors()->attach(
+                $authors->random(rand(1, 3))->pluck('id')
+            );
+        });
     }
 }
